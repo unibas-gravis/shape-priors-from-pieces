45a0b2fa6533e434f86bc9bd6e1ad0084ff965ce
dennis.madsen <dennis.madsen@unibas.ch>
2020-10-02 13:50:58 +0200

    removed double naming entry from log visualization

--- src/main/scala/apps/hands/VisualizeLog.scala
+++ src/main/scala/apps/hands/VisualizeLog.scala
@@ -27,7 +27,6 @@ object VisualizeLog extends App {
   scalismo.initialize()
 
   val logPath = Paths.handLogPath
-  val targetpartialname = "hand-1_thumb_15"
 
   val modelFile = new File(Paths.handPath, "hand2D_gp_s25_s50_s120_per.h5")
   val model = StatisticalModelIO.readStatisticalLineMeshModel2D(modelFile).get
@@ -53,7 +52,7 @@ object VisualizeLog extends App {
   Visualization2DHelper.show2DLineMesh(ui, targetGroup, targetGTMesh, "grount-truth").opacity = 0f
 
   val burnInPhase = 200
-  val logFile = new File(logPath, targetpartialname + ".json")
+  val logFile = new File(logPath, partialName + ".json")
   val log = LogHelper2D(logFile, model, burnInPhase)
 
   val rndShapes = log.sampleMeshes2D(takeEveryN = 20, total = 10000, randomize = true)
